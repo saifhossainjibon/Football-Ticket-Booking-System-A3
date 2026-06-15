31515c1a57c055d725eca8b708919f388ec37bde
saifhossainjibon <saifhossainjibon@gmail.com>
2026-06-15 19:47:30 +0600

    Query 3 Done

--- query.sql
+++ query.sql
@@ -49,3 +49,8 @@ WHERE tournament_category= 'Champions League' AND match_status= 'Available';
 -- Query 2: Search for all users whose full names start with 'Tanvir' or contain the phrase 'Haque' (case-insensitive).
 SELECT user_id, full_name, email FROM Users
 WHERE full_name ILIKE 'Tanvir%' or full_name ILIKE '%Haque';
+
+-- Query 3: Retrieve all booking records where the payment status is missing (NULL), replacing the empty result with 'Action Required'.
+SELECT booking_id, user_id, match_id, COALESCE(payment_status,'Action Required') AS systematic_status FROM Bookings
+WHERE payment_status is null;
+
